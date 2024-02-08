bb2afd179646794a5372be311db163b74122330d
Anurag Ingle <anuragingle2002@gmail.com>
2024-02-08 22:17:15 +0530

    Spring Framework Added

---
+++ demo/src/test/java/com/facultyapplication/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facultyapplication.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
